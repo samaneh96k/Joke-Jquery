b9a7bfc27ded37f26dbf2e3a476d6f5470a5727e
samaneh karimi <samanehkarimi17@gmail.com>
2022-10-28 17:39:35 +0330

    add web pack and bundling

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import $ from 'jquery';
+import './styles.css'
+const url = "https://icanhazdadjoke.com/";
+
+const btn = $(".btn");
+const result = $(".result");
+
+const fetchDadJoke = async () => {
+  result.textContent = "Loading...";
+
+  $.ajax({
+    url: url,
+    type: "GET",
+    dataType: "json",
+    success: function(res) {
+      console.log(res);
+      result.text(res.joke);
+    },
+    error: function() {
+      alert("There was an error.");
+    }
+  });
+
+};
+btn.click(fetchDadJoke);
+fetchDadJoke();
